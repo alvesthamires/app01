ac7b85921ffd74386b7d2c6aa2878617c56ac469
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-08-06 10:19:39 -0300

    atualizar estrutura do projetos e signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [ Produto ],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-    produtos = [
+  produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 179 },
     { nome: 'Teclado', preco: 210 },
     { nome: 'Caixa de som', preco: 997 },
     { nome: 'Fone', preco: 70 },
-  ];
+  ]);
 
-    exibirProduto(nome: string) {
+  exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
   }
 
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Mouse Pad', preco: 50 }]);
+  }
 }
