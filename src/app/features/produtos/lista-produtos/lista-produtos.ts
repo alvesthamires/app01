be58a2f543ab174cd76d69298af778b43d41702b
Thamires Alves da Silva <apenasthamires@gmail.com>
2026-08-06 11:39:32 -0300

    writable e computed signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
 
 @Component({
@@ -7,6 +7,8 @@ import { Produto } from '../produto/produto';
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
+
+//produtos = signal ([]); é um writable signal
 export class ListaProdutos {
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
@@ -16,12 +18,24 @@ export class ListaProdutos {
     { nome: 'Fone', preco: 70 },
   ]);
 
+  totalProdutos = computed(() => this.produtos().length);
+
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
+
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
     // Aqui você pode atualizar o estado, abrir modal, etc.
   }
 
+  // update -> adiciona um produto novo ao writable signal
   adicionarProduto() {
     this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Mouse Pad', preco: 50 }]);
+  }
+
+  // set -> substitui um produto no writable signal
+  substituirProdutos() {
+    this.produtos.set([{ nome: 'Produto novo', preco: 999 }]);
   }
 }
